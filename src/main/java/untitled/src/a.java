f58fa8bf21e64e361dfae870410f2d18754e5309
Nilanjan <nilanjan01>
2018-10-28 16:01:25 +0530

    commit

---
+++ src/main/java/untitled/src/a.java
@@ -0,0 +1,7 @@
+package untitled.src;
+public class a {
+    public static void main (String args[]){
+        System.out.println("wef");
+        System.out.println("w2ef");
+    }
+}
---
+++ src/main/java/untitled/src/a9.java
@@ -0,0 +1,8 @@
+package untitled.src;
+
+public class a9 {
+
+    public static void main(String[] args){
+        System.out.println("er");
+    }
+}
